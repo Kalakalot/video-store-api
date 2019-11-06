b3abdf18a99fd6a96ee4267aebdf8121e78df5f0
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-06 12:24:17 -0800

    Created test fixtures file for movies and started making movie tests pass.

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,35 +1,35 @@
 require "test_helper"
 
 describe Movie do
-
-  let (:firstmovie) {movies(:one)}
+  
+  let (:movie) {movies(:bride)}
   it "can be created" do
-    expect(movies.valid?).must_equal true
+    expect(movie.valid?).must_equal true
   end
-
-  it "requires name, registered_at and postal_code" do
-    required_fields = [:name, :registered_at, :postal_code, :phone]
+  
+  it "requires title, overview, release_date, and inventory" do
+    required_fields = [:title, :overview, :release_date, :inventory]
     
-
     required_fields.each do |field|
-       peanut[field] = nil
-
-       expect(firstmovie.valid?).must_equal false
-
-       firstmovie.reload
+      movie[field] = nil
+      
+      expect(movie.valid?).must_equal false
+      
+      movie.reload
     end
-
+    
     # As with all Rails projects, model testing is a requirement. 
     # You should have at least one positive and one negative test case for each relation, 
     # validation, and custom function you add to your models.
   end
+  
   describe "validations" do
-
+    
     it "can be valid" do
       is_valid = users(:valid_user_with_username).valid?
       assert( is_valid )
     end
-
+    
     it "is invalid if there is no username" do
       user = users(:invalid_user_without_username)
       is_valid = user.valid?
@@ -37,5 +37,5 @@ describe Movie do
     end
     
   end
-
+  
 end
