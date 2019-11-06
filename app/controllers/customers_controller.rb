de2fe403a3b88dd4b4a11b6afac85bff0aae61ed
Sara Shah Baig <sarashahbaig@gmail.com>
2019-11-05 16:58:56 -0800

    edited

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,24 @@
 class CustomersController < ApplicationController
+    def index
+        @pets = Pet.all
+        # pets = Pet.all.as_json(only: [:name, :age, :human])
+        # render json: pets, status: :ok
+        # render json: { ready_for_lunch: "yassss" }, status: :ok
+    end
+    
+    def show
+    pet = Pet.find_by(id: params[:id]).as_json(only: [:name, :age, :human])
+    render json: pet, status: :ok
+    # render json: { ready_for_lunch: "yassss" }, status: :ok
+    end
+
+    
+    
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :age, :human)
+    end
+
 end
