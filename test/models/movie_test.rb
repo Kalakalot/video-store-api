c803eea3645951b3b048db21233e5b65c917ca9e
Sara Shah Baig <sarashahbaig@gmail.com>
2019-11-06 07:42:43 -0800

    route added for customer

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 describe Movie do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  let (:firstmovie) {movies(:one)}
+  it "can be created" do
+    expect(movies.valid?).must_equal true
+  end
+
+  it "requires name, registered_at and postal_code" do
+    required_fields = [:name, :registered_at, :postal_code, :phone]
+    
+
+    required_fields.each do |field|
+       peanut[field] = nil
+
+       expect(firstmovie.valid?).must_equal false
+
+       firstmovie.reload
+    end
+
+    # As with all Rails projects, model testing is a requirement. 
+    # You should have at least one positive and one negative test case for each relation, 
+    # validation, and custom function you add to your models.
+  end
+  describe "validations" do
+
+    it "can be valid" do
+      is_valid = users(:valid_user_with_username).valid?
+      assert( is_valid )
+    end
+
+    it "is invalid if there is no username" do
+      user = users(:invalid_user_without_username)
+      is_valid = user.valid?
+      refute( is_valid )
+    end
+    
+  end
+
 end
