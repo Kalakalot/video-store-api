812b4714cea45e3f7d73a88934f81e57341879ed
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-05 16:58:29 -0800

    Added routes and index action for Movie. /movies works on localhost but is only showing movie id.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+  KEYS = [:id, :name, :registered_at, :postal_code, :phone]
+  
+  def index
+    movies = Movie.all
+    render :json => movies.as_json(only: KEYS), status: :ok
+  end
+  
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render :json => movie.as_json(only: KEYS) #status: :ok
+      return
+    else
+      render json: {"errors" =>["not found"]}, status: :not_found
+      # we need to specify response for "not found", otherwise Rails will just return default response of success
+      # we can use the errors key for future error messages 
+      return
+    end
+  end
+  
+  private
+  def movie_params
+    params.require(:movie).permit(:id, :name, :registered_at, :postal_code, :phone)
+  end
 end
