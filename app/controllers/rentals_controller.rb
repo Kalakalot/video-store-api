40158ef1d2c0ea7a12255b72a64abf9da90ada33
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-07 15:03:21 -0800

    Added fixture for rentals and relationship test for customer model.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,27 +1,13 @@
 class RentalsController < ApplicationController
-  
-def check_out
-
+    
     def checkout
-
-<<<<<<< HEAD
-end
-
-def check_in
-
-
-end
-
-private
-def rental_params
-params.require(:rental).permit(:customer_id, :movie_id)
-=======
+        
         customer_id = params[:customer_id]
         movie_id = params[:movie_id]
-
+        
         customer = Customer.find_by(id: customer_id)
         movie = Movie.find_by(id: movie_id)
-
+        
         if movie.nil? || customer.nil?
             render_error("Sorry, no record found for this movie or customer!")
             return
@@ -33,25 +19,25 @@ params.require(:rental).permit(:customer_id, :movie_id)
             movie.rentals << rental
             movie.available_inventory = movies_available - 1
             movie.save!
-
+            
             customer.rentals << rental
             customer.movies_checked_out_count += 1
             customer.save!
         else
             render_error("Sorry, '#{movie.title}' has no copies available to rent")
         end
-
+        
         render json: movie, status: :ok
     end
-
-
+    
+    
     def checkin
         customer_id = params[:customer_id]
         movie_id = params[:movie_id]
-
+        
         customer = Customer.find_by(id: customer_id)
         movie = Movie.find_by(id: movie_id)
-
+        
         if movie.nil? || customer.nil?
             render_error("Sorry, no record found for this movie or customer!")
             return
@@ -63,28 +49,29 @@ params.require(:rental).permit(:customer_id, :movie_id)
             movie.rentals.delete(rental)
             movie.available_inventory = movies_available + 1
             movie.save!
-
+            
             customer.rentals.delete(rental)
             customer.movies_checked_out_count -= 1
             customer.save!
         else
             render_error
         end
-
+        
         render json: movie, status: :ok
     end
-
+    
     private 
+    
     def rentals_params
         params.require(:rentals).permit(:customer_id, :movie_id)
     end
-
-    private
+    
     def render_error(message)
         render json: {
             ok: false,
             errors: message
-        }, status: :bad_request
+            }, status: :bad_request
+        end
     end
->>>>>>> 20a35aec23f8652eb22da77f14ea79f093658cc1
-end
+    
+    
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,4 +4,38 @@ describe Customer do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  
+  describe "relations" do
+    it "has a list of rentals" do
+      princess_bride = movies(:bride)
+      swamp_thing = movies(:swamp)
+      customer = customers(:one)
+      
+      _(customer).must_respond_to :rentals
+      customer.rentals.each do |rental|
+        _(rental).must_be_kind_of Rental
+      end
+    end
+  end
+  
+  describe "validations" do
+    # it "requires a username" do
+    #   user = User.new
+    #   user.valid?.must_equal false
+    #   user.errors.messages.must_include :username
+    # end
+    
+    # it "requires a unique username" do
+    #   username = "test username"
+    #   user1 = User.new(username: username)
+    
+    #   # This must go through, so we use create!
+    #   user1.save!
+    
+    #   user2 = User.new(username: username)
+    #   result = user2.save
+    #   result.must_equal false
+    #   user2.errors.messages.must_include :username
+    # end
+  end
 end
