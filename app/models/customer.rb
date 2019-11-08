d9870d3ac6a3ab92257b1bac25761313baa72fad
Sara Shah Baig <sarashahbaig@gmail.com>
2019-11-08 13:32:46 -0800

    edited

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
     has_many :rentals
+
+    validates :name, presence: true
+
+    validates :address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :postal_code, presence: true
+    validates :phone, presence: true
+
+
 end
