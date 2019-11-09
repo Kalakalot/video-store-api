d92d0cb9c0cb74e044a1636ec7347d97418756d3
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-08 16:25:39 -0800

    Added negative tests for customer model.

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -15,31 +15,40 @@ describe Customer do
       customer.rentals.each do |rental|
         _(rental).must_be_kind_of Rental
       end
+      
     end
   end
   
   describe "validations" do
     before do 
-      @customer = Customer.new
+      @invalid_customer = Customer.create
     end
     
-    it "requires a name and a registered_at" do
-      _(@customer.valid?).must_equal false
-      _(@customer.errors.messages).must_include :name
-      _(@customer.errors.messages).must_include  :registered_at
+    it "creates a customer if all required columns are present" do
+      
+      expect{
+        Customer.create(name: "Testy", registered_at: Time.now, address: "123 Main St.", city: "Anytown", state: "Alabama", postal_code: "12334", phone: "218-332-1234")
+      }.must_differ "Customer.count", 1
+      
     end
     
-    it "requires an address, city, state, and postal code" do
-      _(@customer.valid?).must_equal false
-      _(@customer.errors.messages).must_include :address
-      _(@customer.errors.messages).must_include :city
-      _(@customer.errors.messages).must_include :state
-      _(@customer.errors.messages).must_include :postal_code
+    it "won't create a customer unless name and registered_at are included" do
+      _(@invalid_customer.valid?).must_equal false
+      _(@invalid_customer.errors.messages).must_include :name
+      _(@invalid_customer.errors.messages).must_include  :registered_at
     end
     
-    it "requires a phone" do
-      _(@customer.valid?).must_equal false
-      _(@customer.errors.messages).must_include :phone
+    it "won't create a customer unless address, city, state, and postal code are provided" do
+      _(@invalid_customer.valid?).must_equal false
+      _(@invalid_customer.errors.messages).must_include :address
+      _(@invalid_customer.errors.messages).must_include :city
+      _(@invalid_customer.errors.messages).must_include :state
+      _(@invalid_customer.errors.messages).must_include :postal_code
+    end
+    
+    it "won't create a customer if phone isn't provided" do
+      _(@invalid_customer.valid?).must_equal false
+      _(@invalid_customer.errors.messages).must_include :phone
     end
     
   end
